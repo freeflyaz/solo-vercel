ec1e894564076d3c7cc8021041e193c692507fe7
Gabe Mata <gabemata@gmail.com>
2024-03-01 11:11:38 +0100

    feat: flip file deleted

--- app/learning/[question]/page.tsx
+++ app/learning/[question]/page.tsx
@@ -23,29 +23,24 @@ export async function generateMetadata(
     console.log('title', data.name);
   } catch (error) {
     console.error("Failed to fetch product:", error);
-    // Handle the error according to your application's needs
-    // For example, you might want to return a default metadata object or rethrow the error
+    
   }
-  // Example metadata generation logic
+
   const metadata: Metadata =  {
     title: data.order + ' - ' + data.name ,
-    // Assuming you would fetch and populate this data correctly
     description: data.order + ' - ' + data.name
   };
 
   return metadata;
 }
 
-// Correct usage of metadata in a React component
+
 export default function Page({ params, searchParams }: Props) {
-  // Render logic based on params and searchParams
-  // Use the Container component here if needed, not in generateMetadata
+
   return (
-    <div>
-      {/* Example rendering based on passed props */}
-      <h1>{params.question}</h1>
-      {/* More rendering logic here */}
+
+
       <Container questionNumber={params.question} lang={searchParams.lang} />
-    </div>
+
   );
 }
