6843707ed0b600ef938f854d66ab6253b6a8b12b
Gabe Mata <gabemata@gmail.com>
2024-03-03 12:41:13 +0100

    feat: get help tomorrow

--- app/ssr/SwitchLang.jsx
+++ app/ssr/SwitchLang.jsx
@@ -7,38 +7,49 @@ import 'flag-icon-css/css/flag-icons.min.css'; // For flag icons
 import { getData } from './service';
 import { cleanUrl, countryToLanguage } from './util';
 
-function SwitchLang({ selectedLanguage, selectedFlag  }) {
+function SwitchLang({ selectedLanguage, selectedFlag }) {
   // Initialize the flags with German as the top flag and the selected flag as the bottom flag
   const [topFlag, setTopFlag] = useState('de');
   const [bottomFlag, setBottomFlag] = useState(selectedFlag);
+  const [switchLanguage, setSwitchLanguage] = useState('de');
 
   const router = useRouter();
+  //console.log('before click: ', topFlag, bottomFlag);
 
   const flip = async () => {
-    
-  
     // Swap the flags
+
+    //console.log('after click: ', topFlag, bottomFlag);
+    //console.log('after click bottom flag: ', bottomFlag);
+
+    // let newLang = '';
+    // console.log(bottomFlag);
+    // if (bottomFlag === 'de') setBottomFlag('us');
+    // else setBottomFlag('de');
+    // console.log(bottomFlag);
+
     setTopFlag(bottomFlag);
     setBottomFlag(topFlag);
+    setSwitchLanguage(bottomFlag);
+    //console.log('switchLanguage: ', switchLanguage);
 
     // alert('selectedLanguage: ' + selectedLanguage);
     // alert('selectedFlag: ' + selectedFlag);
     // alert('topFlag: ' + topFlag);
     // alert('bottomFlag: ' + bottomFlag);
 
-    const languageCode = countryToLanguage[bottomFlag];
-  
+    const languageCode = countryToLanguage[switchLanguage];
+    console.log('languageCode: ', languageCode);
     // Fetch data for the new language if necessary
     // Assuming getData is needed for something like updating the UI
     const data = await getData(1, languageCode);
-  
+
     // Assuming you need to do something with the data, like clean the URL
     const pushUrl = `/ssr/1-${cleanUrl(data.name)}?lang=${languageCode}`;
-  
+
     // Navigate to the new URL
     router.push(pushUrl);
   };
-  
 
   return (
     <button
@@ -47,8 +58,12 @@ function SwitchLang({ selectedLanguage, selectedFlag  }) {
       // disabled={selectedLanguage === 'de' && selectedFlag === 'de'}
     >
       <PiArrowBendUpLeft className={styles.arrowLeft} />
-      <div className={`flag-icon flag-icon-${topFlag} ${styles.flagRight}`}></div>
-      <div className={`flag-icon flag-icon-${bottomFlag} ${styles.flagLeft}`}></div>
+      <div
+        className={`flag-icon flag-icon-${topFlag} ${styles.flagRight}`}
+      ></div>
+      <div
+        className={`flag-icon flag-icon-${bottomFlag} ${styles.flagLeft}`}
+      ></div>
       <PiArrowBendDownRight className={styles.arrowRight} />
     </button>
   );
