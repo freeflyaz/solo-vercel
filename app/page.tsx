b72e6281258444fc2f2f4d2b28739c57056c4ea6
Gabe Mata <gabemata@gmail.com>
2024-03-05 08:04:21 +0100

    fix: code review

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
-'use client'
-import { Box, Flex, Text, Button, useColorModeValue, Spacer } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-} from '@chakra-ui/react';
-import ContainerQuiz from "./components/ContainerQuiz"; 
-import NavBar from "./components/NavBar";
-import Image from 'next/image'
-
+'use client';
+import ContainerQuiz from './components/ContainerQuiz';
+import NavBar from './components/NavBar';
 
 export default function Home() {
-  const bgColor = useColorModeValue('gray.100', 'gray.900');
-  const color = useColorModeValue('black', 'white');
   return (
     <>
-    <NavBar />
-   
-     <ContainerQuiz />  
+      <NavBar />
+
+      <ContainerQuiz />
     </>
   );
 }
-
-
